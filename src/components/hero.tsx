b2ed577ab837286973317211407010552da27998
Thant Htet Aung <47569203+thanthtetaung4@users.noreply.github.com>
2025-10-14 15:56:08 +0800

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -27,9 +27,9 @@ export function JobCard() {
 								description: "Goodev",
 								duration: "Present",
 								lists: ["Consult",
-											"Designe",
-											"Develope",
-											"Delivere"
+											"Design",
+											"Develop",
+											"Deliver"
 										],
 								content:"Based on the client needs"
 							},
