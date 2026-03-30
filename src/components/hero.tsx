a60bd778bbdc9a3ddf48cfc3fc1f6cf9b425f6b1
Thant Htet Aung <thant.htetaung@visier.com>
2026-03-30 21:59:04 +0800

    work exp and new project added

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,8 @@
-"use client"
+"use client";
 
-import { useTheme } from "next-themes"
-import useScreenSize from "./hook/useScreenSize"
-import { useState, useEffect } from "react"
-
+import { useTheme } from "next-themes";
+import useScreenSize from "./hook/useScreenSize";
+import { useState, useEffect } from "react";
 
 import {
   Card,
@@ -11,128 +10,150 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { WordRotate } from "@/components/ui/word-rotate"
+} from "@/components/ui/card";
+import { WordRotate } from "@/components/ui/word-rotate";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { ShinyButton } from "@/components/ui/shiny-button"
-import { MagicCard } from "@/components/ui/magic-card"
-import Link from "next/link"
+import { ShinyButton } from "@/components/ui/shiny-button";
+import { MagicCard } from "@/components/ui/magic-card";
+import Link from "next/link";
 
 export function JobCard() {
-	const { theme } = useTheme()
+  const { theme } = useTheme();
 
-	const experiences = [
-							{
-								title: "Founder & Developer",
-								description: "Goodev",
-								duration: "Present",
-								lists: ["Consult",
-											"Design",
-											"Develop",
-											"Deliver"
-										],
-								content:"Based on the client needs"
-							},
-							{
-								title: "Tech Lead",
-								description: "SCS@PSBA",
-								duration: "Dec - 2023 => Dec - 2024",
-								lists: ["Led tech team",
-											"Curated workshops & focus groups",
-											"Developed SCS@PSBA the website",
-											"Handled administrative tasks"
-										],
-							}
-						]
+  const experiences = [
+    {
+      title: "AI Software Engineer Intern",
+      description: "Visier Inc.",
+      duration: "Present",
+      lists: [
+        "Optimized 'Vee' Chatbot",
+        "Performance Benchmarking",
+        "Systematic Validation",
+      ],
+      content:
+        "Focused on enhancing Visier’s AI capabilities for workforce analytics.",
+    },
+    {
+      title: "Founder & Developer",
+      description: "Goodev",
+      duration: "Present",
+      lists: ["Consult", "Design", "Develop", "Deliver"],
+      content: "Based on the client needs",
+    },
+    {
+      title: "Tech Lead",
+      description: "SCS@PSBA",
+      duration: "Dec - 2023 => Dec - 2024",
+      lists: [
+        "Led tech team",
+        "Curated workshops & focus groups",
+        "Developed SCS@PSBA the website",
+        "Handled administrative tasks",
+      ],
+    },
+  ];
 
-	return (
-		<section id="exp">
-			<h2 className="text-2xl font-bold text-black dark:text-white">Experiences</h2>
-			<div className="grid gap-4 mt-4 w-ful lg:grid-cols-2">
-				{
-					experiences.map(({title, description, duration, lists, content}, index) => (
-						<Card key={index} className="h-full w-full max-w-sm md:max-w-none border-none p-0 shadow-none">
-							<MagicCard
-							gradientColor={theme === "dark" ? "#262626" : "#D9D9D955"}
-							className="p-0 h-full"
-							>
-								<CardHeader className="border-border border-b p-4 [.border-b]:pb-4">
-									<CardTitle>{title}</CardTitle>
-									<CardDescription>
-										<div className="flex justify-between">
-											<div>{description}</div>
-											<div>{duration}</div>
-										</div>
-
-									</CardDescription>
-								</CardHeader>
-								<CardContent className="p-4">
-									<ul className="list-disc px-2.5">
-									{lists.map((item, index) => (
-										<li key={index}>{item}</li>
-									))}
-									</ul>
-									<div className="px-2.5">
-										{content && content}
-										</div>
-								</CardContent>
-							</MagicCard>
-						</Card>
-					))
-				}
-			</div>
-		</section>
-		);
-	}
+  return (
+    <section id="exp">
+      <h2 className="text-2xl font-bold text-black dark:text-white">
+        Experiences
+      </h2>
+      <div className="grid gap-4 mt-4 w-ful lg:grid-cols-2">
+        {experiences.map(
+          ({ title, description, duration, lists, content }, index) => (
+            <Card
+              key={index}
+              className="h-full w-full max-w-sm md:max-w-none border-none p-0 shadow-none"
+            >
+              <MagicCard
+                gradientColor={theme === "dark" ? "#262626" : "#D9D9D955"}
+                className="p-0 h-full"
+              >
+                <CardHeader className="border-border border-b p-4 [.border-b]:pb-4">
+                  <CardTitle>{title}</CardTitle>
+                  <CardDescription>
+                    <div className="flex justify-between">
+                      <div>{description}</div>
+                      <div>{duration}</div>
+                    </div>
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="p-4">
+                  <ul className="list-disc px-2.5">
+                    {lists.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
+                  <div className="px-2.5">{content && content}</div>
+                </CardContent>
+              </MagicCard>
+            </Card>
+          ),
+        )}
+      </div>
+    </section>
+  );
+}
 
 export function Hero() {
-	const { width } = useScreenSize();
-	const [ml, setMl] = useState(0);
+  const { width } = useScreenSize();
+  const [ml, setMl] = useState(0);
 
-	useEffect(() => {
-		if (!width) return;
+  useEffect(() => {
+    if (!width) return;
 
-		let newMl = 0;
-		if (width >= 1536) {
-			newMl = -width * 0.04;
-		} else if (width >= 810) {
-			newMl = -width * 0.04;
-		} else {
-			newMl = 0;
-		}
+    let newMl = 0;
+    if (width >= 1536) {
+      newMl = -width * 0.04;
+    } else if (width >= 810) {
+      newMl = -width * 0.04;
+    } else {
+      newMl = 0;
+    }
 
-		setMl(newMl);
-	}, [width]);
+    setMl(newMl);
+  }, [width]);
 
-	return (
-		<section id="hero">
-			<div className="grid gap-2 md:grid-cols-4 md:justify-between md:items-start lg:grid-cols-12">
-				<p className="text-3xl font-boldtext-white inline-block md:leading-[-2.7] lg:col-span-2">Hi 👋, I am </p>
-				<div className="md:col-span-3 lg:col-span-10" style={{ marginLeft: ml }} id="word">
-					<WordRotate
-						className="text-4xl font-bold text-black dark:text-white inline-block"
-						words={["Thant", "A Developer"]}/>
-				</div>
-				<p className="mt-5 text-lg md:col-span-full md:text-xl">Full-stack software developer skilled in system programming and full-stack development,
-					with experience leading projects, freelancing, releasing tool and winning hackathons.</p>
-			</div>
-			<div className="mt-5 flex justify-around">
-				<div className="w-1/2 flex justify-around items-center">
-					<Link href="https://github.com/thanthtetaung4" target="blank">
-						<FaGithub className="text-2xl md:text-3xl"/>
-					</Link>
-					<Link href="https://www.linkedin.com/in/thant-htet-aung/" target="blank">
-						<FaLinkedin className="text-2xl md:text-3xl"/>
-					</Link>
-				</div>
-				<ShinyButton link="./resume.pdf">
-					Get My Resume
-				</ShinyButton>
-			</div>
-			<div className="mt-5">
-				<JobCard/>
-			</div>
-		</section>
-	)
+  return (
+    <section id="hero">
+      <div className="grid gap-2 md:grid-cols-4 md:justify-between md:items-start lg:grid-cols-12">
+        <p className="text-3xl font-boldtext-white inline-block md:leading-[-2.7] lg:col-span-2">
+          Hi 👋, I am{" "}
+        </p>
+        <div
+          className="md:col-span-3 lg:col-span-10"
+          style={{ marginLeft: ml }}
+          id="word"
+        >
+          <WordRotate
+            className="text-4xl font-bold text-black dark:text-white inline-block"
+            words={["Thant", "A Developer"]}
+          />
+        </div>
+        <p className="mt-5 text-lg md:col-span-full md:text-xl">
+          Full-stack software developer skilled in system programming and
+          full-stack development, with experience leading projects, freelancing,
+          releasing tool and winning hackathons.
+        </p>
+      </div>
+      <div className="mt-5 flex justify-around">
+        <div className="w-1/2 flex justify-around items-center">
+          <Link href="https://github.com/thanthtetaung4" target="blank">
+            <FaGithub className="text-2xl md:text-3xl" />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/thant-htet-aung/"
+            target="blank"
+          >
+            <FaLinkedin className="text-2xl md:text-3xl" />
+          </Link>
+        </div>
+        <ShinyButton link="./resume.pdf">Get My Resume</ShinyButton>
+      </div>
+      <div className="mt-5">
+        <JobCard />
+      </div>
+    </section>
+  );
 }
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   SiReact,
@@ -11,7 +11,10 @@ import {
   SiGoogle,
   SiC,
   SiLinux,
+  SiLangchain,
+  SiOllama,
 } from "react-icons/si";
+import { RiSupabaseLine } from "react-icons/ri";
 import {
   Card,
   CardContent,
@@ -19,106 +22,133 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { BorderBeam } from "@/components/ui/border-beam"
+} from "@/components/ui/card";
+import { BorderBeam } from "@/components/ui/border-beam";
 import { ShinyButton } from "./ui/shiny-button";
 import useScreenSize from "./hook/useScreenSize";
 
 function ProjectCards() {
-	const {width} = useScreenSize();
+  const { width } = useScreenSize();
 
-	let duration = 4;
-	if (width >= 1024) {
-		duration = 6;
-	} else if (width >= 768) {
-		duration = 8;
-	}
+  let duration = 4;
+  if (width >= 1024) {
+    duration = 6;
+  } else if (width >= 768) {
+    duration = 8;
+  }
 
-	const projects = [
-	{
-		title: "AnalytixNexa",
-		description:
-		"Open-source data analysis web app",
-		content:
-		"Empowers small businesses to visualize, analyze, and understand their data through an intuitive dashboard with Firebase authentication and real-time analytics.",
-		techStack: [<SiReact key={"react"} fontSize={24}/>, <SiMui key={"mui"} fontSize={24}/>, <SiFlask key={"flask"} fontSize={24}/>, <SiFirebase key={"firebase"} fontSize={24}/>, <SiPython key={"py"} fontSize={24}/>],
-	},
-	{
-		title: "PaletteGen",
-		description:
-		"AI-powered color palette generator",
-		content:
-		"Generates aesthetic color palettes using Google Gemini AI and serves designers with instant creative inspiration.",
-		techStack: [<SiNextdotjs key={"next"} fontSize={24}/>, <SiDeno key={"deno"} fontSize={24}/>, <SiFlask key={"flask"} fontSize={24}/>, <SiPython key={"py"} fontSize={24}/>, <SiGoogle key={"g"} fontSize={24}/>],
-	},
-	{
-		title: "Mini Shell (Bash Clone)",
-		description:
-		"Bash-like shell written in pure C",
-		content:
-		"Implements core shell features such as pipes, redirections, and environment variables — entirely built from scratch in C.",
-		techStack: [<SiC key={"C"} fontSize={24}/>, <SiLinux key={"Linux"} fontSize={24}/>],
-	},
-	{
-		title: "Mini RT (Ray Tracing Engine)",
-		description:
-		"Mini ray-tracing engine implemented in C",
-		content:
-		"Renders realistic 3D scenes using ray tracing with lighting, shadows, and reflections via MiniLibX.",
-		techStack: [<SiC key={"C"} fontSize={24}/>],
-	},
-	];
+  const projects = [
+    {
+      title: "Knoverse",
+      description:
+        "Knoverse is a collaborative platform that enables teams to engage in AI-powered chat system driven by their internal documents.",
+      content:
+        "It enables users to ask natural-language questions about their team’s uploaded documents and receive accurate, contextual answers powered by a Retrieval-Augmented Generation (RAG) system. The platform also provides administrators with full control over teams, users, permissions, and activity analytics, allowing organizations to understand engagement and knowledge usage across teams.",
+      techStack: [
+        <SiNextdotjs key={"next"} fontSize={24} />,
+        <SiPython key={"py"} fontSize={24} />,
+        <SiLangchain key={"lc"} fontSize={24} />,
+        <SiOllama key={"ollama"} fontSize={24} />,
+        <RiSupabaseLine key={"supabase"} fontSize={24} />,
+      ],
+    },
+    {
+      title: "AnalytixNexa",
+      description: "Open-source data analysis web app",
+      content:
+        "Empowers small businesses to visualize, analyze, and understand their data through an intuitive dashboard with Firebase authentication and real-time analytics.",
+      techStack: [
+        <SiReact key={"react"} fontSize={24} />,
+        <SiMui key={"mui"} fontSize={24} />,
+        <SiFlask key={"flask"} fontSize={24} />,
+        <SiFirebase key={"firebase"} fontSize={24} />,
+        <SiPython key={"py"} fontSize={24} />,
+      ],
+    },
+    {
+      title: "PaletteGen",
+      description: "AI-powered color palette generator",
+      content:
+        "Generates aesthetic color palettes using Google Gemini AI and serves designers with instant creative inspiration.",
+      techStack: [
+        <SiNextdotjs key={"next"} fontSize={24} />,
+        <SiDeno key={"deno"} fontSize={24} />,
+        <SiFlask key={"flask"} fontSize={24} />,
+        <SiPython key={"py"} fontSize={24} />,
+        <SiGoogle key={"g"} fontSize={24} />,
+      ],
+    },
+    {
+      title: "Mini Shell (Bash Clone)",
+      description: "Bash-like shell written in pure C",
+      content:
+        "Implements core shell features such as pipes, redirections, and environment variables — entirely built from scratch in C.",
+      techStack: [
+        <SiC key={"C"} fontSize={24} />,
+        <SiLinux key={"Linux"} fontSize={24} />,
+      ],
+    },
+    {
+      title: "Mini RT (Ray Tracing Engine)",
+      description: "Mini ray-tracing engine implemented in C",
+      content:
+        "Renders realistic 3D scenes using ray tracing with lighting, shadows, and reflections via MiniLibX.",
+      techStack: [<SiC key={"C"} fontSize={24} />],
+    },
+  ];
 
-	return (
-		<div className="mt-5">
-			<div className="grid gap-6 md:grid-cols-2">
-			{
-				projects.map(({title, description, content, techStack}, index) => (
-					<Card key={index} className="relative overflow-hidden">
-						<CardHeader>
-							<CardTitle>{title}</CardTitle>
-							<CardDescription className="flex justify-between">
-								<div>{description}</div>
-							</CardDescription>
-						</CardHeader>
-						<CardContent>
-							{content}
-						</CardContent>
-						<CardFooter>
-							<div className="flex justify-between overflow-hidden" style={{ width: `${24 * (techStack.length * 1.5)}px` }}>
-								{techStack}
-							</div>
-						</CardFooter>
-						 <BorderBeam
-						duration={duration}
-						size={400}
-						borderWidth={2}
-						className="from-transparent via-blue-500 to-transparent"
-						/>
-						<BorderBeam
-						duration={duration}
-						delay={3}
-						size={400}
-						borderWidth={2}
-						className="from-transparent via-blue-500 to-transparent"
-						/>
-					</Card>
-				))
-			}
-			</div>
-		</div>
-	)
+  return (
+    <div className="mt-5">
+      <div className="grid gap-6 md:grid-cols-2">
+        {projects.map(({ title, description, content, techStack }, index) => (
+          <Card key={index} className="relative overflow-hidden">
+            <CardHeader>
+              <CardTitle>{title}</CardTitle>
+              <CardDescription className="flex justify-between">
+                <div>{description}</div>
+              </CardDescription>
+            </CardHeader>
+            <CardContent>{content}</CardContent>
+            <CardFooter>
+              <div
+                className="flex justify-between overflow-hidden"
+                style={{ width: `${24 * (techStack.length * 1.5)}px` }}
+              >
+                {techStack}
+              </div>
+            </CardFooter>
+            <BorderBeam
+              duration={duration}
+              size={400}
+              borderWidth={2}
+              className="from-transparent via-blue-500 to-transparent"
+            />
+            <BorderBeam
+              duration={duration}
+              delay={3}
+              size={400}
+              borderWidth={2}
+              className="from-transparent via-blue-500 to-transparent"
+            />
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export function Projects ()  {
-
-	return (
-		<section className="mt-5" id="projects">
-			<h2 className="text-2xl font-bold text-black dark:text-white">Projects</h2>
-			<ProjectCards/>
-			<div className="flex justify-center mt-4">
-				<ShinyButton link="https://github.com/thanthtetaung4?tab=repositories"><span className="text-lg font-bold">More On My GitHub</span></ShinyButton>
-			</div>
-		</section>
-	)
+export function Projects() {
+  return (
+    <section className="mt-5" id="projects">
+      <h2 className="text-2xl font-bold text-black dark:text-white">
+        Projects
+      </h2>
+      <ProjectCards />
+      <div className="flex justify-center mt-4">
+        <ShinyButton link="https://github.com/thanthtetaung4?tab=repositories">
+          <span className="text-lg font-bold">More On My GitHub</span>
+        </ShinyButton>
+      </div>
+    </section>
+  );
 }
